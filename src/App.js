ad776b65edca285f7a7181eb8581fe106b26a16c
kashmirkhunt <kashmirakhunt@gmail.com>
2022-05-26 08:51:31 +0530

    hospital

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Auth from './Components/Auth/Auth';
+
+
+
+
+function App() {
+  return (
+    <>
+    <Auth/>
+    </>
+  
+  );
+}
+
+export default App;
+
---
+++ src/Components/Auth/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Auth(props) {
+          return (
+                    <div>
+                         <p>helklo world</p>   
+                    </div>
+          );
+}
+
+export default Auth;
