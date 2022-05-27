c73e5f624f98438d63c646563dde37508eb11b21
kashmirkhunt <kashmirakhunt@gmail.com>
2022-05-27 08:48:26 +0530

    27-5-2022

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Auth from './Components/Auth/Auth';
+import Home from './Container/Home/Home';
+import Department from './Components/Department/Department';
+import Doctors from './Components/Doctors/Doctors';
+import About from './Components/About/About';
+import Contact from './Components/Contact/Contact';
+import Auth from './Container/Auth/Auth';
+import Header from './Components/Header/Header';
+import { Switch,Route } from 'react-router-dom';
+import Footer from './Components/Footer/Footer';
 
 
 
@@ -8,8 +16,19 @@ import Auth from './Components/Auth/Auth';
 function App() {
   return (
     <>
-    <Auth/>
-    </>
+    <Header/>
+   <Switch>
+   <Route exact path={"/"}components={Home}/>
+    <Route exact path={"/department"}components={Department}/>
+      <Route exact path={"/doctors"}components={Doctors}/>
+      <Route exact path={"/about"}components={About}/>
+      <Route exact path={"/contact"}components={Contact}/>
+      <Route exact path={"/danger"}components={Auth}/>
+      
+   </Switch>
+    <Footer/>  
+   </>
+   
   
   );
 }
